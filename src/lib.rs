22a7009e918620b523ea06e54ca75275eabe8027
Ingy döt Net <ingy@ingy.net>
2026-08-18 12:56:44 -0400

    Release 0.10.7

--- src/lib.rs
+++ src/lib.rs
@@ -136,7 +136,7 @@
 //! `Vec<u8>`.
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/yaml_serde/0.10.6")]
+#![doc(html_root_url = "https://docs.rs/yaml_serde/0.10.7")]
 #![deny(missing_docs, unsafe_op_in_unsafe_fn)]
 // Suppressed clippy_pedantic lints
 #![allow(
